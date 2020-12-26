f213bff8760c173d483fcd3d990526e010270241
bartech <bartekspitza@hotmail.com>
2020-12-26 18:55:55 +0100

    perft now runs several positions

--- src/perft.c
+++ src/perft.c
@@ -3,19 +3,43 @@
 #include "board.h"
 
 typedef unsigned long long u64;
-
-
 u64 perft(Board board, int depth, bool divide);
+
+char* FENS[250] = {
+  START_FEN,
+  "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq"
+};
+int NUM_FENS = 2;
+
+// Starting from depth 1
+u64 EXPECTED_RESULTS[][6] = {
+  {20, 400 , 8902 , 197281 , 4865609  , 119060324  },
+  {48, 2039, 97862, 4085603, 193690690, 8031647685 },
+};
+
 
 int main() {
     initMoveGenerationTables();
-    Board board;
-    setFen(&board, START_FEN);
 
-    int depth = 6;
-    printf("Perft %d\n", depth);
-    u64 nodes = perft(board, depth, true);
-    printf("\nNodes searched: %llu\n", nodes);
+    int depth = 3;
+    printf("Depth %d\n\n", depth);
+
+    for (int i = 0; i < NUM_FENS;i++) {
+      char* fen = FENS[i];
+
+      Board board;
+      setFen(&board, fen);
+
+      u64 nodes = perft(board, depth, false);
+      u64 expected = EXPECTED_RESULTS[i][depth-1];
+      bool matches = nodes == expected;
+
+      printf(matches ? 
+      "Success: " : 
+      "Fail:    ");
+
+      printf("%llu/%llu %s\n", nodes, expected, fen);
+    }
 
     return 0;
 }
