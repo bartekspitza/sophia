2f56d6c8a22cbd1a692e12d35ef707b22df5e209
bartech <bartekspitza@hotmail.com>
2020-12-27 16:55:12 +0100

    added more fens to perf, passes perf 5 on all

--- src/perft.c
+++ src/perft.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include "board.h"
 
 typedef unsigned long long u64;
@@ -7,22 +8,31 @@ u64 perft(Board board, int depth, bool divide);
 
 char* FENS[250] = {
   START_FEN,
-  "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq"
+  "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq",
+  "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w",
+  "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+  "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  ",
+  "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w"
 };
-int NUM_FENS = 2;
+int NUM_FENS = 6;
 
 // Starting from depth 1
 u64 EXPECTED_RESULTS[][6] = {
   {20, 400 , 8902 , 197281 , 4865609  , 119060324  },
   {48, 2039, 97862, 4085603, 193690690, 8031647685 },
+  {14, 191 , 2812 , 43238  , 674624   , 11030083   },
+  {6 , 264 , 9467 , 422333 , 15833292 , 706045033  },
+  {44, 1486, 62379, 2103487, 89941194              },
+  {46, 2079, 89890, 3894594, 164075551, 6923051137 },
 };
 
 
 int main() {
     initMoveGenerationTables();
 
-    int depth = 3;
+    int depth = 5;
     printf("Depth %d\n\n", depth);
+
 
     for (int i = 0; i < NUM_FENS;i++) {
       char* fen = FENS[i];
@@ -30,7 +40,7 @@ int main() {
       Board board;
       setFen(&board, fen);
 
-      u64 nodes = perft(board, depth, false);
+      u64 nodes = perft(board, depth, true);
       u64 expected = EXPECTED_RESULTS[i][depth-1];
       bool matches = nodes == expected;
 
@@ -63,6 +73,7 @@ u64 perft(Board board, int depth, bool divide) {
     u64 count = perft(cpy, depth-1, false);
     if (divide) {
       char san[6];
+      memset(&san, 0, sizeof(san));
       moveToSan(moves[i], san);
       printf("%s: %llu\n", san, count);
     }
