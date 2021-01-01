b72ceea17437282e1c6d8ba5115c7c1ea8f1ec6e
bartech <bartekspitza@hotmail.com>
2021-01-01 18:09:23 +0100

    added back piece square tables

--- src/evaluation.c
+++ src/evaluation.c
@@ -16,65 +16,67 @@ int PIECE_VALUES[] = {
    -2000
 };
 
+
 int PAWN_W_PST[] = {
     0,  0,  0,  0,  0,  0,  0,  0,
-    50, 50, 50, 50, 50, 50, 50, 50,
-    10, 10, 20, 30, 30, 20, 10, 10,
-    5,  5, 10, 25, 25, 10,  5,  5,
-    0,  0,  0, 20, 20,  0,  0,  0,
-    5, -5,-10,  0,  0,-10, -5,  5,
     5, 10, 10,-20,-20, 10, 10,  5,
+    5, -5,-10,  0,  0,-10, -5,  5,
+    0,  0,  0, 20, 20,  0,  0,  0,
+    5,  5, 10, 25, 25, 10,  5,  5,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    50, 50, 50, 50, 50, 50, 50, 50,
     0,  0,  0,  0,  0,  0,  0,  0
 };
 int KNIGHT_W_PST[] = {
     -50,-40,-30,-30,-30,-30,-40,-50,
-    -40,-20,  0,  0,  0,  0,-20,-40,
-    -30,  0, 10, 15, 15, 10,  0,-30,
-    -30,  5, 15, 20, 20, 15,  5,-30,
-    -30,  0, 15, 20, 20, 15,  0,-30,
-    -30,  5, 10, 15, 15, 10,  5,-30,
     -40,-20,  0,  5,  5,  0,-20,-40,
+    -30,  5, 10, 15, 15, 10,  5,-30,
+    -30,  0, 15, 20, 20, 15,  0,-30,
+    -30,  5, 15, 20, 20, 15,  5,-30,
+    -30,  0, 10, 15, 15, 10,  0,-30,
+    -40,-20,  0,  0,  0,  0,-20,-40,
     -50,-40,-30,-30,-30,-30,-40,-50
 };
+
 int BISHOP_W_PST[] = {
     -20,-10,-10,-10,-10,-10,-10,-20,
-    -10,  0,  0,  0,  0,  0,  0,-10,
-    -10,  0,  5, 10, 10,  5,  0,-10,
-    -10,  5,  5, 10, 10,  5,  5,-10,
-    -10,  0, 10, 10, 10, 10,  0,-10,
-    -10, 10, 10, 10, 10, 10, 10,-10,
     -10,  5,  0,  0,  0,  0,  5,-10,
+    -10, 10, 10, 10, 10, 10, 10,-10,
+    -10,  0, 10, 10, 10, 10,  0,-10,
+    -10,  5,  5, 10, 10,  5,  5,-10,
+    -10,  0,  5, 10, 10,  5,  0,-10,
+    -10,  0,  0,  0,  0,  0,  0,-10,
     -20,-10,-10,-10,-10,-10,-10,-20
 };
 int ROOK_W_PST[] = {
-    0,  0,  0,  0,  0,  0,  0,  0,
-    5, 10, 10, 10, 10, 10, 10,  5,
+     0,  0,  5,  10, 10, 5,  0,  0,
     -5,  0,  0,  0,  0,  0,  0, -5,
     -5,  0,  0,  0,  0,  0,  0, -5,
     -5,  0,  0,  0,  0,  0,  0, -5,
     -5,  0,  0,  0,  0,  0,  0, -5,
     -5,  0,  0,  0,  0,  0,  0, -5,
-    0,  0,  5,  10,  10,  5,  0,  0
+     5,  10, 10, 10, 10, 10, 10, 5,
+     0,  0,  0,  0,  0,  0,  0,  0,
 };
 int QUEEN_W_PST[] = {
-    -20,-10,-10, -5, -5,-10,-10,-20,
-    -10,  0,  0,  0,  0,  0,  0,-10,
-    -10,  0,  5,  5,  5,  5,  0,-10,
-    -5,  0,  5,  5,  5,  5,  0, -5,
-     0,  0,  5,  5,  5,  5,  0, -5,
-    -10,  5,  5,  5,  5,  5,  0,-10,
-    -10,  0,  5,  0,  0,  0,  0,-10,
     -20,-10,-10, -5, -5,-10,-10,-20
+    -10,  0,  5,  0,  0,  0,  0,-10,
+    -10,  5,  5,  5,  5,  5,  0,-10,
+     0,  0,  5,  5,  5,  5,  0, -5,
+    -5,  0,  5,  5,  5,  5,  0, -5,
+    -10,  0,  5,  5,  5,  5,  0,-10,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -20,-10,-10, -5, -5,-10,-10,-20,
 };
 int KING_W_PST[] = {
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -30, -40, -40, -50, -50, -40, -40, -30,
-    -20, -30, -30, -40, -40, -30, -30, -20,
-    -10, -20, -20, -20, -20, -20, -20, -10,
+     20,  30,  10,  0,   0,   10,  30,  20,
      20,  20,  0,   0,   0,   0,   20,  20,
-     20,  30,  10,  0,   0,   10,  30,  20
+    -10, -20, -20, -20, -20, -20, -20, -10,
+    -20, -30, -30, -40, -40, -30, -30, -20,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
 };
 
 int PAWN_B_PST[64];
@@ -106,43 +108,63 @@ int evaluate(Board board, int result) {
     int eval = 0;
 
     while (board.pawn_W) {
+        int sq = __builtin_ctzll(board.pawn_W);
         eval += PIECE_VALUES[PAWN_W];
+        eval += PAWN_W_PST[sq];
         board.pawn_W &= board.pawn_W -1 ;
     }
     while (board.knight_W) {
+        int sq = __builtin_ctzll(board.knight_W);
         eval += PIECE_VALUES[KNIGHT_W];
+        eval += KNIGHT_W_PST[sq];
         board.knight_W &= board.knight_W -1 ;
     }
     while (board.bishop_W) {
+        int sq = __builtin_ctzll(board.bishop_W);
         eval += PIECE_VALUES[BISHOP_W];
+        eval += BISHOP_W_PST[sq];
         board.bishop_W &= board.bishop_W -1 ;
     }
     while (board.rook_W) {
+        int sq = __builtin_ctzll(board.rook_W);
         eval += PIECE_VALUES[ROOK_W];
+        eval += ROOK_W_PST[sq];
         board.rook_W &= board.rook_W -1 ;
     }
     while (board.queen_W) {
+        int sq = __builtin_ctzll(board.queen_W);
         eval += PIECE_VALUES[QUEEN_W];
+        eval += QUEEN_W_PST[sq];
         board.queen_W &= board.queen_W -1 ;
     }
     while (board.pawn_B) {
+        int sq = __builtin_ctzll(board.pawn_B);
         eval += PIECE_VALUES[PAWN_B];
+        eval -= PAWN_B_PST[sq];
         board.pawn_B &= board.pawn_B -1 ;
     }
     while (board.knight_B) {
+        int sq = __builtin_ctzll(board.knight_B);
         eval += PIECE_VALUES[KNIGHT_B];
+        eval -= KNIGHT_B_PST[sq];
         board.knight_B &= board.knight_B -1 ;
     }
     while (board.bishop_B) {
+        int sq = __builtin_ctzll(board.bishop_B);
         eval += PIECE_VALUES[BISHOP_B];
+        eval -= BISHOP_B_PST[sq];
         board.bishop_B &= board.bishop_B -1 ;
     }
     while (board.rook_B) {
+        int sq = __builtin_ctzll(board.rook_B);
         eval += PIECE_VALUES[ROOK_B];
+        eval -= ROOK_B_PST[sq];
         board.rook_B &= board.rook_B -1 ;
     }
     while (board.queen_B) {
+        int sq = __builtin_ctzll(board.queen_B);
         eval += PIECE_VALUES[QUEEN_B];
+        eval -= QUEEN_B_PST[sq];
         board.queen_B &= board.queen_B -1 ;
     }
 
