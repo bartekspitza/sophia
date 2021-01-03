06eed38597fcd3b563599dc1f262f5cb91d56203
bartech <bartekspitza@hotmail.com>
2021-01-03 03:22:30 +0100

    changed profiling.c to instead perform a single depth 6 search

--- src/profiling.c
+++ src/profiling.c
@@ -21,28 +21,17 @@ int main(void) {
     Board board;
     setFen(&board, START_FEN);
 
-    Move moves[250];
-    int cmoves = pseudoLegalMoves(board, moves);
-    int res = result(board, moves, cmoves);
 
-    int depth = 4;
+    int depth = 6;
+    int nodesSearched = 0;
+    Move bestMove;
+    PVline pvLine;
 
     clock_t start = clock();
-    while (res == UN_DETERMINED) {
-
-        int nodesSearched = 0;
-
-        Move bestMove;
-        PVline pvLine;
-        int eval = search(board, depth, &bestMove, &nodesSearched, &pvLine);
-
-        pushMove(&board, bestMove);
-        
-        cmoves = pseudoLegalMoves(board, moves);
-        res = result(board, moves, cmoves);
-    }
-
+    int eval = search(board, depth, &bestMove, &nodesSearched, &pvLine);
     double elapsed = (double) (clock() - start) / CLOCKS_PER_SEC;
+    
+
     printf("Elapsed: %.2fs\n", elapsed);
     return 0;
 }
