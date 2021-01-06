d8770e0b80f600cd64b760c1a289f228e4f0bfa3
bartech <bartekspitza@hotmail.com>
2021-01-06 22:31:48 +0100

    fixes #7

--- src/zobrist.c
+++ src/zobrist.c
@@ -1,10 +1,6 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "zobrist.h"
-#include "movegen.h"
 
-
-unsigned int randomU32();
 Bitboard randomBitboard();
 
 Bitboard PIECES[12][64];
@@ -13,8 +9,10 @@ Bitboard CASTLING[16];
 Bitboard WHITE_TO_MOVE;
 
 Bitboard hash(Board board) {
+    Bitboard hash = 0LL;
+
     // Castling rights
-    Bitboard hash = CASTLING[board.castling];
+    hash ^= CASTLING[board.castling];
 
     // Pieces
     for (int i = 0; i < 12; i++) {
@@ -41,39 +39,17 @@ Bitboard hash(Board board) {
 }
 
 void initZobrist() {
-    // Pieces
     for (int i = 0; i < 12; i++) for (int j = 0; j < 64;j++) PIECES[i][j] = randomBitboard();
-
-    // En passant
     for (int i = 0; i < 64; i++) EN_PASSANT[i] = randomBitboard();
-
-    // Castling
     for (int i = 0; i < 16; i++) CASTLING[i] = randomBitboard();
-
     WHITE_TO_MOVE = randomBitboard();
 }
 
-unsigned int RANDOM_STATE = 1804289383;
-
-unsigned int randomU32() {
-    unsigned int number = RANDOM_STATE;
-    number ^= number << 13;
-    number ^= number >> 17;
-    number ^= number << 5;
-    RANDOM_STATE = number;
-    return number;
+Bitboard randomBitboard(void) {
+    Bitboard r = 0;
+    for (int i=0; i < 64; i++) {
+        Bitboard tmp = (Bitboard) rand() % 2;
+        r |= tmp << i;
+    }
+    return r;
 }
-
-Bitboard randomBitboard() {
-    Bitboard n1, n2, n3, n4;
-    
-    // init random numbers slicing 16 bits from MS1B side
-    n1 = (Bitboard)(randomU32()) & 0xFFFF;
-    n2 = (Bitboard)(randomU32()) & 0xFFFF;
-    n3 = (Bitboard)(randomU32()) & 0xFFFF;
-    n4 = (Bitboard)(randomU32()) & 0xFFFF;
-    
-    return n1 | (n2 << 16) | (n3 << 32) | (n4 << 48);
-}
-
-
